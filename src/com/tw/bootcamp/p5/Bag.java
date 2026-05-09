c282d005d4402a6a0ba10efe08b40da82badcb3c
kiranmailalam <kiranmailalam@gmail.com>
2026-05-09 15:13:33 +0530

    categorizes balls in the bag

--- src/com/tw/bootcamp/p5/Bag.java
+++ src/com/tw/bootcamp/p5/Bag.java
@@ -1,19 +1,25 @@
 package com.tw.bootcamp.p5;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class Bag {
     private final int capacity;
+    private final Map<Ball, Integer> balls;
     private int ballCount;
 
     public Bag() {
         this.ballCount = 0;
         this.capacity = 12;
+        this.balls = new HashMap<>();
     }
 
-    public boolean add() {
+    public boolean add(Ball ball) {
         if (ballCount >= capacity) {
             return false;
         }
 
+        balls.compute(ball, (k, val) -> val == null ? 1 : val + 1);
         ballCount++;
         return true;
     }
--- tests/com/tw/bootcamp/p5/BagTest.java
+++ tests/com/tw/bootcamp/p5/BagTest.java
@@ -9,15 +9,15 @@ public class BagTest {
     @Test
     void shouldAddBallToTheBag() {
         Bag bag = new Bag();
-        assertTrue(bag.add());
+        assertTrue(bag.add(Ball.GREEN));
     }
 
     @Test
     void shouldNotAddBallToTheBagIfItIsFull() {
         Bag bag = new Bag();
         for (int i = 0; i < 12; i++) {
-            bag.add();
+            bag.add(Ball.GREEN);
         }
-        assertFalse(bag.add());
+        assertFalse(bag.add(Ball.GREEN));
     }
 }
